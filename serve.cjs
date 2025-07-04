3c6cf8a61c447cb15feb8af833de8bcaa654e375
Gerardo <VampyrTempestmail.com>
2025-07-03 23:29:11 -0500

    fix: Prueba de despliegue Railways-03 con uso de extension cjs

---
+++ serve.cjs
@@ -0,0 +1,16 @@
+// serve.cjs
+const { exec } = require("child_process");
+
+const port = process.env.PORT || 8080;
+const command = `npx serve -s dist -l ${port}`;
+
+console.log(`👉 Starting frontend on port ${port}...`);
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.error("❌ Error starting serve:", err);
+    process.exit(1);
+  }
+
+  if (stdout) console.log(stdout);
+  if (stderr) console.error(stderr);
+});
